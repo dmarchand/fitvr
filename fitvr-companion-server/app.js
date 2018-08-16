df27f21770710f3d7ca595d2060405dbbfdf8c62
Dan Marchand <dmarchand@fitbit.com>
2018-08-16 15:46:53 -0400

    HR logging

--- fitvr-companion-server/app.js
+++ fitvr-companion-server/app.js
@@ -1,11 +1,34 @@
-const express = require('express')
+const express = require('express'),
+                fs = require('fs')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const HR_HISTORY_PATH = '../common/heartRateHistory'
+var writer
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!')
+  cleanUp()
+  writer = fs.createWriteStream(HR_HISTORY_PATH, {
+            flags: 'a+'
+        })
+})
 
 app.get('/submitHeartRate', function (req, res) {
-  console.log(req.query.hr);
-  res.send('hello world')
+  let heartRate = req.query.hr
+  console.log('Got heart rate: ' + heartRate)
+  writer.write(heartRate + '\n')
+  res.send('Success!')
+
 })
+
+function cleanUp() {
+  console.log('Process terminating.')
+  fs.truncate(HR_HISTORY_PATH, 0, function(){
+    console.log('Cleaned up HR history')
+    process.exit()
+  })
+}
+
+process.on('exit', cleanUp)
+process.on('SIGTERM', cleanUp)
+process.on('SIGINT', cleanUp)
